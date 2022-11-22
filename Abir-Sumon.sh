bc2ea31040e5f88b00de40972d384af520b50e52
wibrwibr <118586650+wibrwibr@users.noreply.github.com>
2022-11-22 23:27:06 +0600

    Create Abir-Sumon.sh

---
+++ Abir-Sumon.sh
@@ -0,0 +1,42 @@
+clear
+
+echo "
+
+ █████╗ ██████╗ ██╗██████╗                         
+██╔══██╗██╔══██╗██║██╔══██╗                        
+███████║██████╔╝██║██████╔╝                        
+██╔══██║██╔══██╗██║██╔══██╗                        
+██║  ██║██████╔╝██║██║  ██║                        
+╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═╝                        
+                                                   
+███████╗██╗   ██╗███╗   ███╗ ██████╗ ███╗   ██╗    
+██╔════╝██║   ██║████╗ ████║██╔═══██╗████╗  ██║    
+███████╗██║   ██║██╔████╔██║██║   ██║██╔██╗ ██║    
+╚════██║██║   ██║██║╚██╔╝██║██║   ██║██║╚██╗██║    
+███████║╚██████╔╝██║ ╚═╝ ██║╚██████╔╝██║ ╚████║    
+╚══════╝ ╚═════╝ ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   
+"
+echo "==================================="
+echo "      tools name : basic pkg install
+"
+echo "      author : Abir
+"
+echo "==================================="
+apt update 
+apt upgrade
+pkg install python -y
+pkg install python2 -y
+pkg install php 
+pip install future
+pkg install git -y
+pkg install figlet -y
+pkg install cmatrix -y
+apt install ruby
+pip2 install mechanize
+pip2 install requests
+pip2 install bs4
+pkg install toilet -y
+pkg install nano -y
+apt install openssh
+apt install wget
+apt instLP
